11fbd0688730a39a9d23937ea89dde1acbaec5b3
Kondwani <kondwanimsiska@gmail.com>
2022-01-24 10:08:00 +0200

    chore(main): configured route handling
    Signed-off-by: Kondwani <kondwanimsiska@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals/categories_screen.dart';
+import './categories_screen.dart';
+import './category_meals_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,21 +13,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meals',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Railway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-            bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-            bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-            subtitle1: const TextStyle(
-                fontSize: 20,
-                fontFamily: 'RobotoCondensed',
-                fontWeight: FontWeight.bold)),
-      ),
-      home: const CategoriesScreen(),
-    );
+        title: 'Meals',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Railway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              subtitle1: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold)),
+        ),
+        home: const CategoriesScreen(),
+        routes: {
+          '/category-meals': (ctx) => CategoryMealsScreen(),
+        });
   }
 }
