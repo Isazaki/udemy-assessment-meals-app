94d549fb847ff18278a44149f4da6414299e020e
Kondwani <kondwanimsiska@gmail.com>
2022-01-25 20:17:01 +0200

    chore(main): created onUnknownRoute as a 404 fall-back page
    Signed-off-by: Kondwani <kondwanimsiska@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Meals',
-        theme: ThemeData(
-          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Railway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              subtitle1: const TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold)),
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
-              .copyWith(secondary: Colors.amber),
-        ),
-        home: const CategoriesScreen(),
-        initialRoute: '/',
-        routes: {
-          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-          MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        });
+      title: 'Meals',
+      theme: ThemeData(
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Railway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            subtitle1: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold)),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+      ),
+      home: const CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => const MealDetailScreen(),
+      },
+      onUnknownRoute: (settings) {
+        // 404 fault back page
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+    );
   }
 }
