89d484fb5afb5e410d9b1f27c6c4e2c5399bd369
Kondwani <kondwanimsiska@gmail.com>
2022-01-29 08:29:37 +0200

    chore(main_drawer):  created drawer widget with text labels, icons and builder functions
    Signed-off-by: Kondwani <kondwanimsiska@gmail.com>

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 import './favorites_screen.dart';
 import './categories_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -34,6 +36,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title'].toString()),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'] as Widget,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
