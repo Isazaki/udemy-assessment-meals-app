193801e4b557806a71099aaee1a8ca5c2add9394
Kondwani <kondwanimsiska@gmail.com>
2022-01-31 14:25:14 +0200

    chore(filtered): restore list tile switch  state using initstate - set active switch color to follow theme - added save filter button
    Signed-off-by: Kondwani <kondwanimsiska@gmail.com>

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -4,7 +4,10 @@ import 'package:meals/widgets/main_drawer.dart';
 class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
 
-  const FiltersScreen({Key? key}) : super(key: key);
+  final Function saveFilters;
+  final Map<String, bool> currentFilters;
+
+  FiltersScreen(this.currentFilters, this.saveFilters);
 
   @override
   State<FiltersScreen> createState() => _FiltersScreenState();
@@ -16,9 +19,19 @@ class _FiltersScreenState extends State<FiltersScreen> {
   bool _vegan = false;
   bool _lactoseFree = false;
 
+  @override
+  initState() {
+    _glutenFree = widget.currentFilters['gluten']!;
+    _lactoseFree = widget.currentFilters['lactose']!;
+    _vegan = widget.currentFilters['vegan']!;
+    _vegetarian = widget.currentFilters['vegetarian']!;
+    super.initState();
+  }
+
   Widget _buildSwitchListTile(String title, String description,
           bool currentValue, Function(bool) updateValue) =>
       SwitchListTile(
+        activeColor: Theme.of(context).colorScheme.secondary,
         title: Text(title),
         subtitle: Text(description),
         value: currentValue,
@@ -28,7 +41,23 @@ class _FiltersScreenState extends State<FiltersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Your Filters')),
+        appBar: AppBar(
+          title: const Text('Your Filters'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: () {
+                final selectedFilters = {
+                  'gluten': _glutenFree,
+                  'lactose': _lactoseFree,
+                  'vegan': _vegan,
+                  'vegetarian': _vegetarian,
+                };
+                widget.saveFilters(selectedFilters);
+              },
+            )
+          ],
+        ),
         drawer: const MainDrawer(),
         body: Column(
           children: <Widget>[
